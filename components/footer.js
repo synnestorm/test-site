758320710dd88acfc7bf33e6ea753047808f19e5
synnestorm <synne.storm@gmail.com>
2025-12-08 13:07:06 +0100

    edit

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return /*HTML*/ `
-  <img src="public/img/circle-footer.png" class="footer-img-desktop" alt="">
-  <img src="public/img/Circles_illustration_mobile.png" class="footer-img-mobile" alt="">
+  <img src="../public/img/circle-footer.png" class="footer-img-desktop" alt="">
+  <img src="../public/img/circle_illustration_mobile.png" class="footer-img-mobile" alt="">
   `;
 }
